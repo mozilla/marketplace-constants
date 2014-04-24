0584d64b2f6487d6a60eda2f4c992e445921eb88
Andy McKay <amckay@mozilla.com>
2014-04-24 16:18:26 -0700

    first commit

---
+++ README.txt
@@ -0,0 +1,16 @@
+Global constants for the Firefox Marketplace, available in both JS and Python.
+
+JSON
+----
+
+The JSON should be a valid object, so that object keys become accessible from
+Python.
+
+Python
+------
+
+Example:
+
+>>> from constants import regions
+>>> regions.AR
+{u'name': u'Argentina', u'default_language': u'es', u'mcc': ...
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name='marketplace-constants',
+    version='0.1',
+    description='Standard constants for the marketplace',
+    long_description=open('README.txt').read(),
+    author='Andy McKay',
+    author_email='andym@mozilla.com',
+    license='BSD',
+    packages=['constants'],
+    url='https://github.com/andymckay/marketplace-constants',
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+    ]
+)

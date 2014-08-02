0557cfff46a413815f86b98c9b3815dd8941de1f
Andy McKay <amckay@mozilla.com>
2014-08-01 17:02:30 -0700

    noodling

--- generate.py
+++ generate.py
@@ -3,6 +3,8 @@ import importlib
 import json
 import os
 
+import requests
+
 py_files = glob.glob('constants/*.py')
 js_files = glob.glob('js/*.json')
 
@@ -16,6 +18,9 @@ def names(files):
 
 
 for filename in py_files:
+    if filename == 'mozilla_languages':
+        raise ValueError('The file mozilla_languages is reserved.')
+
     filename = name(filename)
     module = 'constants.' + filename
     mod = importlib.import_module(module)
@@ -34,10 +39,16 @@ for filename in py_files:
     print '{0} file: {1}'.format(change, output)
     json.dump(export, open(output, 'w'), indent=2)
 
-
 diff = set(names(js_files)).difference(set(names(py_files)))
 
 for filename in diff:
     # Clean up any missing files.
     print 'Deleting file: {0}'.format(filename)
     os.remove(os.path.join('js', filename + '.json'))
+
+remote_lang = 'http://svn.mozilla.org/libs/product-details/json/languages.json'
+data = requests.get(remote_lang)
+open('js/mozilla_languages.js').write(data.json)
+open('js/mozilla_languages.py').write(
+    'LANGUAGES = {0}'.format(pprint.pformat(data.json))
+)

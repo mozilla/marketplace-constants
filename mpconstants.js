442c7d548a03cb216503a4239d9abf47820e1c29
Andy McKay <amckay@mozilla.com>
2014-08-06 16:52:54 -0700

    add in countries and currencies

--- mpconstants.js
+++ mpconstants.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
 
 module.exports = function(file) {
-  return JSON.parse(fs.readFileSync(__dirname + '/js/' + file + '.json'));
+  return JSON.parse(fs.readFileSync(__dirname + '/json/' + file + '.json'));
 };

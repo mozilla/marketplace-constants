63c8fc9cd7c2f5fa82cfd5c8fe5123175c52109a
Mathieu Pillard <m@virgule.net>
2015-04-29 18:48:23 +0200

    v0.10.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='marketplace-constants',
-    version='0.9.1',
+    version='0.10.0',
     description='Standard constants for the marketplace',
     long_description=open('README.md').read(),
     author='Andy McKay',
